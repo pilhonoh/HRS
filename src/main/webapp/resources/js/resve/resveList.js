156ae580f25197356bc39bc06b139c95d395be2f
P149080 <seschan@saerom.co.kr>
2019-09-03 13:49:50 +0900

    예약리스트 (예약취소, 대기취소 기능 개발 중)

--- src/main/webapp/resources/js/resve/resveList.js
+++ src/main/webapp/resources/js/resve/resveList.js
@@ -65,6 +65,7 @@ var resveList = {
 	combobox: {
 		deleteStsOption: function() {
 			$('select#stsCombo option[value="STS00"]').remove(); //'미예약' 삭제
+			$('select#stsCombo option[value="STS99"]').remove(); //'근무취소' 삭제
 		}
 	},
 	
@@ -81,6 +82,9 @@ var resveList = {
 			statusCode: '', //상태
 			startRow: 0 //조회 시작할 ROW
 		},
+		
+		//현재 조회된 데이터 저장
+		dataList: [],
 		
 		//예약 목록 조회
 		selectResveList: function() {
@@ -99,6 +103,8 @@ var resveList = {
 					console.log('resveList', res);
 					if (res.status === 200) {
 						deferred.resolve(res);
+						resveList.list.dataList = res.list;
+						
 					} else {
 						deferred.reject("");
 					}
@@ -122,6 +128,7 @@ var resveList = {
 				var resultList = result.list;
 				var resveListHtml = [];
 				var btnText = '';
+				var btnClass = '';
 				var resveDt = '';
 				
 				resveList.paging.params.totalCount = result.customs.totalCount;
@@ -130,8 +137,10 @@ var resveList = {
 					var stsCode = resultList[i].LAST_STTUS_CODE;
 					if (stsCode == 'STS01') {
 						btnText = '예약취소';
+						btnClass = 'resveCancelBtn';
 					} else if (stsCode == 'STS03') {
 						btnText = '대기취소';
+						btnClass = 'waitCancelBtn';
 					}
 					
 					var resve_de = resultList[i].RESVE_DE;
@@ -147,7 +156,7 @@ var resveList = {
 					resveListHtml.push('	<td>' + resultList[i].STTUS_NM + '</td>');
 					resveListHtml.push('	<td>');
 					if (stsCode == 'STS01' || stsCode == 'STS03') {
-						resveListHtml.push('		<button class="t-btn">' + btnText + '</button>');
+						resveListHtml.push('		<button class="t-btn ' + btnClass + '" data-resveno="' + resultList[i].RESVE_NO + '">' + btnText + '</button>');
 					}
 					resveListHtml.push('	</td>');
 					resveListHtml.push('</tr>');
@@ -156,20 +165,41 @@ var resveList = {
 				$('tbody#resveList').html(resveListHtml.join(''));
 				resveList.paging.renderPaging();
 				
+				resveList.button.resveCancelBtnEvent();
+				resveList.button.waitCancelBtnEvent();
+				
 			});
+		},
+		
+		//현재 리스트에서 예약번호로 해당 ROW 의 데이터를 가져옴
+		getRowData: function(resveNo) {
+			var rowDataList = resveList.list.dataList;
+			var rowData;
+			
+			for (var i in rowDataList) {
+				if (rowDataList[i].RESVE_NO == resveNo) {
+					rowData = rowDataList[i];
+					break;
+				}
+			}
+			
+			return rowData;
 		}
 
 	},
 	
 	
 	paging: {
-		
 		//페이징 처리를 위한 파라미터
 		params: {
-			totalCount: '' //list 의 전체 row count
+			totalCount: null, //list 의 전체 row count
+			first: null, //첫페이지
+			last: null, //마지막페이지
+			prev: null, //이전페이지
+			next: null //다음페이지
 		},
 		
-		//페이징 영역 생성
+		//페이지 이동 영역 생성
 		renderPaging: function() {
 			
 			var currentIndex = resveList.list.params.pageNo; //현재 페이지 위치
@@ -187,21 +217,33 @@ var resveList = {
 			var prev = (parseInt((currentIndex-1)/10)*10) - 9 > 0 ? (parseInt((currentIndex-1)/10)*10) - 9 : 1; 
 			var next = (parseInt((currentIndex-1)/10)+1) * 10 + 1 < totalIndexCount ? (parseInt((currentIndex-1)/10)+1) * 10 + 1 : totalIndexCount;
 			
-
+			resveList.paging.params.first = first;
+			resveList.paging.params.last = last;
+			resveList.paging.params.prev = prev;
+			resveList.paging.params.next = next;
 			
+			/*
 			if (totalIndexCount > 10) { //전체 인덱스가 10이 넘을 경우, first + prev 버튼
-				preStr += '<a href="#none" class="first"><img src="' + IMG + '/common/btn_first.gif"></a>'
-					   +  '<a href="#none" class="prev"><img src="' + IMG + '/common/btn_prev.gif"></a>';
+				preStr += '<a href="#none" class="first" id="firstBtn"><img src="' + IMG + '/common/btn_first.gif"></a>'
+					   +  '<a href="#none" class="prev" id="prevBtn"><img src="' + IMG + '/common/btn_prev.gif"></a>';
 			} else if (totalIndexCount <=10 && totalIndexCount > 1) { //전체 인덱스가 10보다 작을 경우, first 버튼
-				preStr += '<a href="#none" class="first"><img src="' + IMG + '/common/btn_first.gif"></a>';
+				preStr += '<a href="#none" class="first" id="firstBtn"><img src="' + IMG + '/common/btn_first.gif"></a>';
 			}
+			*/
+			preStr += '<a href="#none" class="first" id="firstBtn"><img src="' + IMG + '/common/btn_first.gif"></a>'
+			   	   +  '<a href="#none" class="prev" id="prevBtn"><img src="' + IMG + '/common/btn_prev.gif"></a>';
 			
+			
+			/*
 			if (totalIndexCount > 10) { //전체 인덱스가 10이 넘을 경우, next + last 버튼
-				postStr += '<a href="#none" class="next"><img src="' + IMG + '/common/btn_next.gif"></a>'
-						+  '<a href="#none" class="last"><img src="' + IMG + '/common/btn_last.gif"></a>';
+				postStr += '<a href="#none" class="next" id="nextBtn"><img src="' + IMG + '/common/btn_next.gif"></a>'
+						+  '<a href="#none" class="last" id="lastBtn"><img src="' + IMG + '/common/btn_last.gif"></a>';
 			} else if (totalIndexCount <=10 && totalIndexCount > 1) { //전체 인덱스가 10보다 작을 경우, last 버튼
-				postStr += '<a href="#none" class="last"><img src="' + IMG + '/common/btn_last.gif"></a>';
-			}			
+				postStr += '<a href="#none" class="last" id="lastBtn"><img src="' + IMG + '/common/btn_last.gif"></a>';
+			}
+			*/
+			postStr += '<a href="#none" class="next" id="nextBtn"><img src="' + IMG + '/common/btn_next.gif"></a>'
+			        +  '<a href="#none" class="last" id="lastBtn"><img src="' + IMG + '/common/btn_last.gif"></a>';
 
 			for (var i=first; i<(first+last); i++) {
 				if (i != currentIndex) {
@@ -213,15 +255,70 @@ var resveList = {
 			
 			$("div#pagingArea").append(preStr + pageNumStr + postStr);
 			
+			resveList.paging.button.allPagingBtnEventBinding();
+		},
+		
+		button: {
+			allPagingBtnEventBinding: function() {
+				resveList.paging.button.pageNumEvent();
+				resveList.paging.button.prevBtnEvent();
+				resveList.paging.button.nextBtnEvent();
+				resveList.paging.button.firstBtnEvent();
+				resveList.paging.button.lastBtnEvent();
+			},
+			
+			pageNumEvent: function() {
+				$('a.num').off();
+				$('a.num').on('click', function() {
+					resveList.list.params.pageNo = parseInt(this.innerHTML);
+					resveList.list.renderResveList();
+				});
+			},
+			
+			prevBtnEvent: function() {
+				$('a#prevBtn').off();
+				$('a#prevBtn').on('click', function() {
+					resveList.list.params.pageNo = resveList.paging.params.prev;
+					resveList.list.renderResveList();
+				});
+			},
+			
+			nextBtnEvent: function() {
+				$('a#nextBtn').off();
+				$('a#nextBtn').on('click', function() {
+					resveList.list.params.pageNo = resveList.paging.params.next;
+					resveList.list.renderResveList();
+				});
+			},
+			
+			firstBtnEvent: function() {
+				$('a#firstBtn').off();
+				$('a#firstBtn').on('click', function() {
+					resveList.list.params.pageNo = resveList.paging.params.first;
+					resveList.list.renderResveList();
+				});
+			},
+			
+			lastBtnEvent: function() {
+				$('a#lastBtn').off();
+				$('a#lastBtn').on('click', function() {
+					resveList.list.params.pageNo = resveList.paging.params.last;
+					resveList.list.renderResveList();
+				});
+			}
 		}
-		
-		
 		
 	},
 	
 	
 	
 	button: {
+		
+		cancelBtnStatus: {
+			type: '', //예약취소(resve) or 대기취소(wait)
+			rowData: {} //취소버튼의 rowData
+		},
+		
 		//조회 버튼 클릭 이벤트
 		listBtnClickEvent: function() {
 			$('button#listBtn').on('click', function(e) {
@@ -241,17 +338,31 @@ var resveList = {
 		
 		//예약취소 버튼 클릭 이벤트
 		resveCancelBtnEvent: function() {
-			$('button .resveCancelBtn').off();
-			$('button .resveCancelBtn').on('click', function(e) {
+			$('button.resveCancelBtn').off();
+			$('button.resveCancelBtn').on('click', function(e) {
+				var btn = $(this);
 				
+				console.log(resveList.list.getRowData(btn.data('resveno')));
+				
+				resveList.button.cancelBtnStatus.type = 'resve';
+				resveList.button.cancelBtnStatus.rowData =  resveList.list.getRowData(btn.data('resveno'));
+				
+				resveList.popup.showCancelPopup();
 			});
 		},
 		
 		//대기취소 버튼 클릭 이벤트
 		waitCancelBtnEvent: function() {
-			$('button .waitCancelBtn').off();
-			$('button .waitCancelBtn').on('click', function(e) {
+			$('button.waitCancelBtn').off();
+			$('button.waitCancelBtn').on('click', function(e) {
+				var btn = $(this);
 				
+				console.log(resveList.list.getRowData(btn.data('resveno')));
+				
+				resveList.button.cancelBtnStatus.type = 'wait';
+				resveList.button.cancelBtnStatus.rowData =  resveList.list.getRowData(btn.data('resveno'));
+				
+				resveList.popup.showCancelPopup();
 			});
 		}
 	},
@@ -286,6 +397,16 @@ var resveList = {
 			resveList.list.params.toDate = todt;
 			
 			return true;
+		}
+	},
+	
+	
+	popup: {
+		showCancelPopup: function() {
+			var rowData = resveList.button.cancelBtnStatus.rowData;
+			
+			
+			e_layer_pop03('layer_pop03');
 		}
 	}
 
