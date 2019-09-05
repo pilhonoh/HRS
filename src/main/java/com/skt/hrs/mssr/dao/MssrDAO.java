fd3bd2060b643760d0fd2ba49e10e995849e20e0
P149080 <seschan@saerom.co.kr>
2019-09-05 15:38:18 +0900

    관리사 스케쥴 (개발 중)

--- src/main/java/com/skt/hrs/mssr/dao/MssrDAO.java
+++ src/main/java/com/skt/hrs/mssr/dao/MssrDAO.java
@@ -33,6 +33,35 @@ public class MssrDAO {
 	public List getMssrList(DataEntity param) {
 		return sqlSession.selectList("mssrMapper.getMssrList", param);
 	}
+	
+	
+	
+	/**
+	 * 
+	 * @설명 : 관리사 스케쥴 리스트 조회 
+	 * @작성일 : 2019.09.05
+	 * @작성자 : LEE.J.H
+	 * @param param
+	 * @return
+	 * @변경이력 :
+	 */
+	public List selectScheduleList(DataEntity param) {
+		return sqlSession.selectList("mssrMapper.selectScheduleList", param);
+	}
+	
+	
+	/**
+	 * 
+	 * @설명 : 관리사 스케쥴 리스트 TOTAL COUNT 조회 
+	 * @작성일 : 2019.09.05
+	 * @작성자 : LEE.J.H
+	 * @param param
+	 * @return
+	 * @변경이력 :
+	 */
+	public int selectScheduleListTotalCount(DataEntity param) {
+		return sqlSession.selectOne("mssrMapper.selectScheduleListTotalCount", param);
+	}
 
 	
 }
