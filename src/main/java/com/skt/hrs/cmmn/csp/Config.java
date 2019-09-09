896ed9bfcfca1442319611e5a384e2101817b850
김대종 <skt.P021090@parter.sk.com>
2019-09-09 16:25:44 +0900

    config 설정 정보 수정

--- src/main/java/com/skt/hrs/cmmn/csp/Config.java
+++ src/main/java/com/skt/hrs/cmmn/csp/Config.java
@@ -7,37 +7,37 @@ import org.springframework.stereotype.Component;
 public class Config {
 	
 	// Domain
-	@Value("${CSP.HOST}")
+	//@Value("${CSP.HOST}")
 	public static String domain;
 
 	// BP API Key
-	@Value("${CSP.API_KEY}")
+	//@Value("${CSP.API_KEY}")
 	public static String apiKey;
 
 	// BP 복호화 키
-	@Value("${CSP.DEC_KEY}")
+	//@Value("${CSP.DEC_KEY}")
 	public static String decryptKey;
 
 	// Encode
-	@Value("${CSP.ENCODE}")
+	//@Value("${CSP.ENCODE}")
 	public static String encode;
 	
-	
+	@Value("#{prop['CSP.HOST']}")
 	public void setDomain(String domain) {
 		Config.domain = domain;
 	}
 
-	
+	@Value("#{prop['CSP.API_KEY']}")
 	public void setApiKey(String apiKey) {
 		Config.apiKey = apiKey;
 	}
 	
-	
+	@Value("#{prop['CSP.DEC_KEY']}")
 	public void setDecryptKey(String decryptKey) {
 		Config.decryptKey = decryptKey;
 	}
 	
-	
+	@Value("#{prop['CSP.ENCODE']}")
 	public void setEncode(String encode) {
 		Config.encode = encode;
 	}
