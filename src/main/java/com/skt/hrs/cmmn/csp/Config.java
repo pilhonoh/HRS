7ba4db1713ddf711f6dcabd15fe978d215149e84
P149080 <seschan@saerom.co.kr>
2019-09-09 12:46:07 +0900

    Config.java 수정 - config.properties 값

--- src/main/java/com/skt/hrs/cmmn/csp/Config.java
+++ src/main/java/com/skt/hrs/cmmn/csp/Config.java
@@ -7,33 +7,37 @@ import org.springframework.stereotype.Component;
 public class Config {
 	
 	// Domain
-	public static String domain = "";
+	@Value("${CSP.HOST}")
+	public static String domain;
 
 	// BP API Key
-	public static String apiKey = "";
+	@Value("${CSP.API_KEY}")
+	public static String apiKey;
 
 	// BP 복호화 키
-	public static String decryptKey = "";
+	@Value("${CSP.DEC_KEY}")
+	public static String decryptKey;
 
 	// Encode
-	public static String encode = "";
+	@Value("${CSP.ENCODE}")
+	public static String encode;
 	
-	@Value("#{config['CSP.HOST']}")
+	
 	public void setDomain(String domain) {
 		Config.domain = domain;
 	}
 
-	@Value("#{config['CSP.API_KEY']}")
+	
 	public void setApiKey(String apiKey) {
 		Config.apiKey = apiKey;
 	}
 	
-	@Value("#{config['CSP.DEC_KEY']}")
+	
 	public void setDecryptKey(String decryptKey) {
 		Config.decryptKey = decryptKey;
 	}
 	
-	@Value("#{config['CSP.ENCODE']}")
+	
 	public void setEncode(String encode) {
 		Config.encode = encode;
 	}
