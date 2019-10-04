6d1b12f2a4a4f9d66ff4786bb708734da5a8e3f4
P149365 <P149365@SKTP149365CVM01.SKT.AD>
2019-10-04 13:14:08 +0900

    배치 hrs.batch=true인 경우에만실행, config 분기

--- src/main/java/com/skt/hrs/batch/BlacklistFilterJob.java
+++ src/main/java/com/skt/hrs/batch/BlacklistFilterJob.java
@@ -24,24 +24,27 @@ public class BlacklistFilterJob {
 	Logger logger = LoggerFactory.getLogger("blacklistFilterJobLogger");
 	
 	protected void execute() {	
-		try {
-			
-			// 블랙리스트 저장
-			int cnt = blacklistService.insertBlacklist(paneltyWeeks).getItem();			
-			logger.info("===> {} {} : {} " ,
-					BlacklistFilterJob.class.getSimpleName(), 
-					"필터링된 블랙리스트 카운트", 
-					cnt);
-			
-			// 노쇼 이력 저장
-			List noshowList = resveStatusService.insertNoShowHist().getList();			
-			logger.info("===> {} {} : {} " ,
-					BlacklistFilterJob.class.getSimpleName(), 
-					"노쇼이력 저장 카운트", 
-					noshowList.size());
-			
-		}catch(Exception e) {
-			logger.error("{} BlacklistFilter Error Message : {} ", BlacklistFilterJob.class.getSimpleName(), e.getMessage());
+		String opt = System.getProperty("hrs.batch");
+		if(opt != null && opt.equals("true")) {
+			try {
+					
+				// 블랙리스트 저장
+				int cnt = blacklistService.insertBlacklist(paneltyWeeks).getItem();			
+				logger.info("===> {} {} : {} " ,
+						BlacklistFilterJob.class.getSimpleName(), 
+						"필터링된 블랙리스트 카운트", 
+						cnt);
+				
+				// 노쇼 이력 저장
+				List noshowList = resveStatusService.insertNoShowHist().getList();			
+				logger.info("===> {} {} : {} " ,
+						BlacklistFilterJob.class.getSimpleName(), 
+						"노쇼이력 저장 카운트", 
+						noshowList.size());
+				
+			}catch(Exception e) {
+				logger.error("{} BlacklistFilter Error Message : {} ", BlacklistFilterJob.class.getSimpleName(), e.getMessage());
+			}
 		}
 	}
 }
